22abe7162e00b87a425b6c0a7acd2c531d954571
Leoesbravo <lescogidobravo@outlook.com>
2022-10-19 17:39:59 -0500

    Add project files.

---
+++ BL/Alumno.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Alumno
+    {
+        public static void Add(ML.Alumno alumno)
+        {
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnectionString()))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.CommandText = "INSERT INTO Alumno (Nombre, ApellidoPaterno, ApellidoMaterno, FechaNacimiento, Sexo) VALUES (@Nombre, @ApellidoPaterno, @ApellidoMaterno, @FechaNacimiento, @Sexo)";
+                        cmd.Connection = context;
+
+                        SqlParameter[] collection = new SqlParameter[5];
+
+                        collection[0] = new SqlParameter("@Nombre", System.Data.SqlDbType.VarChar);
+                        collection[0].Value = alumno.Nombre;
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ PL/Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public class Alumno //solicitar información, recompilar información, capturar información 
+    {
+        public static void Add()
+        {
+            ML.Alumno alumno = new ML.Alumno();
+
+            Console.WriteLine("Inserte el nombre del alumno");
+            alumno.Nombre = Console.ReadLine();
+            
+            Console.WriteLine("Inserte el apellido paterno del alumno");
+            alumno.ApellidoPaterno = Console.ReadLine();
+            
+            Console.WriteLine("Inserte el apellido materno del alumno");
+            alumno.ApellidoMaterno = Console.ReadLine();
+
+            Console.WriteLine("Inserte la fecha de nacimiento del alumno");
+            alumno.FechaNacimiento = Console.ReadLine();
+
+            Console.WriteLine("Inserte el sexo del alumno");
+            alumno.Sexo = Console.ReadLine();
+
+            BL.Alumno.Add(alumno);
+
+        }
+    
+    }
+}
